87812aa5712f62f2605a7ce3b00feaeacdc54d1b
Tierra <tierrarucker@ymail.com>
2021-08-31 21:53:28 -0400

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -15,40 +15,37 @@ class CampsiteInfo extends Component {
     )
   }
   
-    renderComments(comments){
-      if (comments){
-        return(
-          <div className="col-md-5 m-1">
-            <h4>Comments</h4>
-            {comments.map(comments => <div key={comments.id}> 
-              <div>
-                {comments.text}
+  // method to render the comments from the CAMPSITE object (in the src folder)
+  // not sure why it is not this.props though???
+  renderComments(comments){
+    if (comments){
+      return(
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map(comment => {
+            return(
+              <div key={comment.id}>
+                <p>{comment.text}<br />
+                -- {comment.author}, {new Intl.DateTimeFormat('en-Us', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
               </div>
-              <div>
-                {comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}
-              </div> 
-                <br />
-              </div>)}
-          </div>
-        );
-      }
+            );
+          })}
+        </div>
+      );
     }
+  }
   
   render(){
-    if(this.campsite){
+    if (this.props.campsite){
       return(
-        <div className="row">
-          {this.renderCampsite(this.campsite)}
-          {this.renderComments(this.props.campsite.comments)}
-
+        <div className='container'>
+          <div className="row">
+            {this.renderCampsite(this.props.campsite)}
+            {this.renderComments(this.props.campsite.comments)}
+          </div>
         </div>
       )
-    } else {
-      return(
-        <div>
-        </div>
-      )
-    }
+    }  return <div />
   }
 }
 
